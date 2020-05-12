a01a4025b5225c7e262862f8c0577e1d01a8e98a
Aniika97 <64832933+Aniika97@users.noreply.github.com>
2020-05-12 21:13:52 +0200

    Inventory eingepflegt
    4 Inventar Felder am unteren rechten Rand. Objekte können noch nicht eingesammelt werden. Kleiner Zwischentest, ob es überhapt funktioniert :)

---
+++ GameDesign_V01/Assets/Scripts/Inventory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public bool[] isFull;
+    // Wenn der Inventar-Slot belegt ist, ist "isFull" = True
+    public GameObject[] slots; 
+}
